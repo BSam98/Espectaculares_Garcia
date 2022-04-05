e81e54848032e1ff08c9296ca3cc8d38d71daab0
Gisselle-Ge <gisean_45@hotmail.com>
2022-04-05 16:19:53 -0500

    cambios

--- app/Views/Administrador/Promociones/agregar_Promociones.php
+++ app/Views/Administrador/Promociones/agregar_Promociones.php
@@ -1,36 +1,34 @@
  <!--MODAL AGREGAR PROMOCIÓN-->
  <div class="modal fade" id="modal_nueva_promocion" style="color:black;">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Agregar Usuarios</h4>
+                <h4 class="modal-title">Agregar Promoción</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
                 <form enctype="multipart/form-data" name="formularioAgregarPromocion" id="formularioAgregarPromocion">
-                    <div class="table table-striped table-responsive">
-                        <table id="agregar-User">
-                            <tbody>
-                                <tr class="fila-User">
-                                    <td>
-                                        <input class="form-control" type ="hidden" id="promocion" name ="promocion" value = ""/>
-                                        <div class="form-group">
-                                            <label for="nombre_promocion">Nombre</label>
-                                            <input class="form-control" type="text"  id="nombre_promocion" required name="nombre_promocion[]" required placeholder="Nombre de la promoción"/>
-                                        </div>
-                                        <div class="form-group" id="area_Cantidad">
-                                        </div>
-                                        <div class="from-group" id="area_Cantidad_Boletos">
-                                        </div>
-                                        <div class="form-group" id="area_Creditos_Cortesia">
-                                        </div>
-                                    </td>
-                                    <td class="del-U"><input type="button"   value="-"/></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <button id="ad-Us" name="ad-Us" type="button" class="btn btn-warning"> + </button>
-                    </div>
+                    <table id="agregar-User" class="table table-responsive table-striped">
+                        <tbody>
+                            <tr class="fila-User">
+                                <td>
+                                    <input class="form-control" type ="hidden" id="promocion" name ="promocion" value = ""/>
+                                    <div class="form-group">
+                                        <label for="nombre_promocion">Nombre</label>
+                                        <input class="form-control" type="text" id="nombre_promocion" required name="nombre_promocion[]" placeholder="Nombre de la promoción"/>
+                                    </div>
+                                    <div class="form-group" id="area_Cantidad">
+                                    </div>
+                                    <div class="from-group" id="area_Cantidad_Boletos">
+                                    </div>
+                                    <div class="form-group" id="area_Creditos_Cortesia">
+                                    </div>
+                                </td>
+                                <td class="del-U"><input type="button" value="-"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button id="ad-Us" name="ad-Us" type="button" class="btn btn-warning"> + </button>
                     <!-- Modal footer -->
                     <div class="modal-footer">
                         <button name="a" type="button" class="btn btn-success" id="a">Agregar </button>
@@ -41,6 +39,7 @@
         </div>
     </div>
 </div>
+
 <script>
     $(function(){
                     // Clona la fila oculta que tiene los campos base, y la agrega al final de la tabla
